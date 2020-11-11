f1ec98aed83542422aa0c97a5b323f54142b1bf2
Der Freimensch <kostrm030102@gmail.com>
2020-11-11 21:30:35 +0300

    add 3-rd lab

---
+++ HLP_LABS/Lab3_9.cpp
@@ -0,0 +1,22 @@
+/*#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	char *ptr;
+	int counter = 0, i = 0, k = 0;
+	ifstream fin;
+	fin.open("help.txt", ios_base:: in);
+	char *buffer = new char[k];
+	char **dictionary = new char* [counter];
+	for (i = 0; i < counter; i++) {
+		dictionary[i] = new char[k];
+	}
+	dictionary = (char**)calloc(counter, sizeof(char*));
+	while (!feof) {
+		while(*ptr != )
+	}
+	
+
+}*/
